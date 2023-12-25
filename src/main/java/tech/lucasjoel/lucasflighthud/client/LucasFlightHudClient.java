70cdc231872c04def741eb5ae70b3d9f74d115d9
LucasJoel1 <ljoel1@outlook.com>
2023-12-25 09:54:18 -0500

    v1.0

--- src/main/java/tech/lucasjoel/lucasflighthud/client/LucasFlightHudClient.java
+++ src/main/java/tech/lucasjoel/lucasflighthud/client/LucasFlightHudClient.java
@@ -1,89 +1,88 @@
 package tech.lucasjoel.lucasflighthud.client;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Heightmap;
 
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class LucasFlightHudClient implements ClientModInitializer {
+    AtomicInteger screenHeight = new AtomicInteger();
+    AtomicInteger screenWidth = new AtomicInteger();
+    AtomicDouble playerCameraYaw = new AtomicDouble();
+    AtomicDouble adjustedPlayerCameraYaw = new AtomicDouble();
+    AtomicDouble playerVelocity = new AtomicDouble();
+    AtomicInteger velCol = new AtomicInteger();
+    AtomicDouble cappedVel = new AtomicDouble();
+    AtomicInteger maxBlockHeight = new AtomicInteger();
+    AtomicReference<String> CompassDirection = new AtomicReference<>("");
+
+
+
 
     @Override
     public void onInitializeClient() {
-
         System.out.println("me do da work");
         Minecraft mc = Minecraft.getInstance();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            assert mc.player != null;
+            try {
+                if (mc.player.isFallFlying()) {
+                    screenHeight.set(mc.getWindow().getGuiScaledHeight());
+                    screenWidth.set(mc.getWindow().getGuiScaledWidth());
+                    playerCameraYaw.set((mc.gameRenderer.getMainCamera().getXRot() + 90) / 180);
+                    adjustedPlayerCameraYaw.set(playerCameraYaw.get() * (screenHeight.get() * 0.666666667) + (screenHeight.get() * 0.166666667));
+                    double xChange = (mc.player.xo - mc.player.getX());
+                    double yChange = (mc.player.yo - mc.player.getY());
+                    double zChange = (mc.player.zo - mc.player.getZ());
+                    playerVelocity.set(Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2) + Math.pow(zChange, 2)) * 20);
+                    if (playerVelocity.get() > 50) {
+                        velCol.set(0x7FF00000);
+                        cappedVel.set(50);
+                    } else {
+                        velCol.set(0x7F008000);
+                        cappedVel.set(playerVelocity.get());
+                    }
+
+                    assert mc.level != null;
+                    BlockPos map = mc.level.getHeightmapPos(Heightmap.Types.WORLD_SURFACE, mc.player.blockPosition());
+                    maxBlockHeight.set((int) mc.player.getY() - map.getY());
+                    if (maxBlockHeight.get() < 0) {
+                        BlockState block = mc.level.getBlockState(new BlockPos((int) mc.player.getX(), (int) mc.player.getY(), (int) mc.player.getZ()));
+                        maxBlockHeight.set(0);
+                        while (block.isAir() && maxBlockHeight.get() < 256) {
+                            maxBlockHeight.getAndIncrement();
+                            block = mc.level.getBlockState(new BlockPos((int) mc.player.getX(), (int) mc.player.getY() - maxBlockHeight.get(), (int) mc.player.getZ()));
+                        }
+                    }
+                }
+            } catch (NullPointerException ignored) {
+
+            }
+        });
+
 
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             assert mc.player != null;
             if (mc.player.isFallFlying()) {
-                int screenHeight = mc.getWindow().getGuiScaledHeight();
-                int screenWidth = mc.getWindow().getGuiScaledWidth();
-                double playerCameraYaw = (mc.gameRenderer.getMainCamera().getXRot() + 90) / 180;
-                double adjustedPlayerCameraYaw = playerCameraYaw * (screenHeight * 0.666666667) + (screenHeight * 0.166666667);
-                drawContext.fill((int) (screenWidth * 0.833333333) + 3, (int) (screenHeight * 0.5), (int) ((screenWidth * 0.833333333) + (screenWidth * 0.083333333333)), (int) adjustedPlayerCameraYaw, 0x7F008000);
-                Util.drawOutlineRect(drawContext, (int) (screenWidth * 0.833333333), (int) (screenHeight * 0.166666667), (int) (screenWidth * 0.083333333333), (int) (screenHeight * 0.666666667), 3, 0xFFFFFFFF);
-                drawContext.drawString(mc.font, String.format("%.2f", (-playerCameraYaw + 0.5) * 2), (int) (screenWidth * 0.833333333) + 8, (int) (screenHeight * 0.666666667) + (int) (screenHeight * 0.166666667) + 10, 0xFFFFFFFF);
-                double xChange = (mc.player.xo - mc.player.getX());
-                double yChange = (mc.player.yo - mc.player.getY());
-                double zChange = (mc.player.zo - mc.player.getZ());
-                double playerVelocity = Math.sqrt(Math.pow(xChange, 2) + Math.pow(yChange, 2) + Math.pow(zChange, 2)) * 20;
-                int velCol;
-                double cappedVel;
-                if (playerVelocity > 50) {
-                    velCol = 0x7FF00000;
-                    cappedVel = 50;
-                } else {
-                    velCol = 0x7F008000;
-                    cappedVel = playerVelocity;
-                }
-                drawContext.fill((int) (screenWidth * 0.185185185) - (int) (screenWidth * 0.083333333333), (int) (screenHeight * 0.166666667) + (int) (screenHeight * 0.666666667) - (int) ((screenHeight * 0.666666667) * (cappedVel / 50)), (int) (screenWidth * 0.185185185), (int) (screenHeight * 0.166666667) + (int) (screenHeight * 0.666666667), velCol);
-                Util.drawOutlineRect(drawContext, (int) (screenWidth * 0.185185185) - (int) (screenWidth * 0.083333333333), (int) (screenHeight * 0.166666667), (int) (screenWidth * 0.083333333333), (int) (screenHeight * 0.666666667), 3, 0xFFFFFFFF);
-                drawContext.drawString(mc.font, String.format("%.2f", playerVelocity), (int) (screenWidth * 0.185185185) - (int) (screenWidth * 0.083333333333) + 8, (int) (screenHeight * 0.666666667) + (int) (screenHeight * 0.166666667) + 10, 0xFFFFFFFF);
+                drawContext.fill((int) (screenWidth.get() * 0.833333333) + 3, (int) (screenHeight.get() * 0.5), (int) ((screenWidth.get() * 0.833333333) + (screenWidth.get() * 0.083333333333)), (int) adjustedPlayerCameraYaw.get(), 0x7F008000);
+                drawContext.fill((int) ((screenWidth.get() * 0.833333333) + (screenWidth.get() * 0.083333333333)), (int) (screenHeight.get() * 0.166666667), screenWidth.get(), (int) (screenHeight.get() * 0.22), 0x4F000000);
+                Util.drawOutlineRect(drawContext, (int) (screenWidth.get() * 0.833333333), (int) (screenHeight.get() * 0.166666667), (int) (screenWidth.get() * 0.083333333333), (int) (screenHeight.get() * 0.666666667), 3, 0xFFFFFFFF);
+                drawContext.drawString(mc.font, String.format("%.2f", (-playerCameraYaw.get() + 0.5) * 2), (int) (screenWidth.get() * 0.833333333) + 8, (int) (screenHeight.get() * 0.666666667) + (int) (screenHeight.get() * 0.166666667) + 10, 0xFFFFFFFF);
+                drawContext.fill((int) (screenWidth.get() * 0.185185185) - (int) (screenWidth.get() * 0.083333333333), (int) (screenHeight.get() * 0.166666667) + (int) (screenHeight.get() * 0.666666667) - (int) ((screenHeight.get() * 0.666666667) * (cappedVel.get() / 50)), (int) (screenWidth.get() * 0.185185185), (int) (screenHeight.get() * 0.166666667) + (int) (screenHeight.get() * 0.666666667), velCol.get());
+                Util.drawOutlineRect(drawContext, (int) (screenWidth.get() * 0.185185185) - (int) (screenWidth.get() * 0.083333333333), (int) (screenHeight.get() * 0.166666667), (int) (screenWidth.get() * 0.083333333333), (int) (screenHeight.get() * 0.666666667), 3, 0xFFFFFFFF);
+                drawContext.drawString(mc.font, String.format("%.2f", playerVelocity.get()), (int) (screenWidth.get() * 0.185185185) - (int) (screenWidth.get() * 0.083333333333) + 8, (int) (screenHeight.get() * 0.666666667) + (int) (screenHeight.get() * 0.166666667) + 10, 0xFFFFFFFF);
 
-                assert mc.level != null;
-                BlockPos map = mc.level.getHeightmapPos(Heightmap.Types.WORLD_SURFACE, mc.player.blockPosition());
-                int maxBlockHeight = (int) mc.player.getY() - map.getY();
 
-                if (maxBlockHeight < 0) {
-                    BlockState block = mc.level.getBlockState(new BlockPos((int) mc.player.getX(), (int) mc.player.getY(), (int) mc.player.getZ()));
-                    maxBlockHeight = 0;
-                    while (block.isAir()) {
-                        maxBlockHeight++;
-                        block = mc.level.getBlockState(new BlockPos((int) mc.player.getX(), (int) mc.player.getY() - maxBlockHeight, (int) mc.player.getZ()));
-
-                    }
-                }
-                drawContext.drawString(mc.font, String.format("%d", maxBlockHeight), (int) (screenWidth * 0.485), (int) (screenHeight * 0.685185185) + 10, 0xFFFFFFFF);
-
-                Util.circle(drawContext, screenWidth / 2 - 1, screenHeight/ 2 - 1, 25, 0xFFFFFFFF);
-                int dir = (int) -(mc.gameRenderer.getMainCamera().getYRot() + 270) % 360;
-                int circleRadius = 25;
-                int centerX = screenWidth / 2;
-                int centerY = screenHeight / 2;
-
-                for (int i = 0; i < 360; i += 90) {
-                    double angle = Math.toRadians(i + dir); // Adjust angle based on dir
-                    int labelRadius = circleRadius + 10; // Distance from the center for labels
-                    int x = (int) Math.round(centerX + labelRadius * Math.cos(angle));
-                    int y = (int) Math.round(centerY + labelRadius * Math.sin(angle));
-
-                    switch (i) {
-                        case 0:
-                            drawContext.drawString(mc.font, "N", x - 4, y - 4, 0xFFFFFFFF);
-                            break;
-                        case 90:
-                            drawContext.drawString(mc.font, "E", x - 4, y - 4, 0xFFFFFFFF);
-                            break;
-                        case 180:
-                            drawContext.drawString(mc.font, "S", x - 4, y - 4, 0xFFFFFFFF);
-                            break;
-                        case 270:
-                            drawContext.drawString(mc.font, "W", x - 4, y - 4, 0xFFFFFFFF);
-                            break;
-                    }
-                }
+                drawContext.drawString(mc.font, "Height: " + maxBlockHeight.get(), (int) (screenWidth.get() * 0.925), (int) (screenHeight.get() * 0.175) + 10, 0xFFFFFFFF);
+                drawContext.drawString(mc.font, "Direction: " + String.valueOf(mc.player.getDirection()).toUpperCase().charAt(0), (int) (screenWidth.get() * 0.925), (int) (screenHeight.get() * 0.175), 0xFFFFFFFF);
             }
         });
     }
--- src/main/java/tech/lucasjoel/lucasflighthud/client/Util.java
+++ src/main/java/tech/lucasjoel/lucasflighthud/client/Util.java
@@ -1,21 +1,13 @@
 package tech.lucasjoel.lucasflighthud.client;
 
+import com.mojang.blaze3d.platform.WindowEventHandler;
 import net.minecraft.client.gui.GuiGraphics;
 
-public class Util  {
+public class Util {
     public static void drawOutlineRect(GuiGraphics g, int x, int y, int width, int height, int thiccness, int color) {
         g.fill(x, y, x + width, y + thiccness, color);
         g.fill(x, y, x + thiccness, y + height, color);
         g.fill(x + width, y, x + width + thiccness, y + height + thiccness, color);
         g.fill(x, y + height, x + width, y + height + thiccness, color);
-    }
-
-    public static void circle(GuiGraphics g, int x, int y, int radius, int color) {
-        for (int i = 0; i < 360; i++) {
-            double angle = i * Math.PI / 180;
-            int x1 = (int) Math.round(x + radius * Math.cos(angle));
-            int y1 = (int) Math.round(y + radius * Math.sin(angle));
-            g.fill(x1, y1, x1 + 1, y1 + 1, color);
-        }
     }
 }
